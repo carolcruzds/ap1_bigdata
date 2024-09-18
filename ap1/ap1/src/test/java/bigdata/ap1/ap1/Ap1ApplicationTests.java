d2219513ed07bed3defba091dac1094c4699167b
CAROLINA CRUZ DOS SANTOS <202201038781@alunos.ibmec.edu.br>
2024-09-18 19:19:50 -0300

    criando

---
+++ ap1/ap1/src/test/java/bigdata/ap1/ap1/Ap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bigdata.ap1.ap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
