a2736b31fac9b72ec9cfc5ee9a393f96b8a64078
carolcruzds <cruzzcarolina27@gmail.com>
2024-10-01 18:21:36 -0300

    uptading

--- gerenciamento/src/test/java/ap1/bigdata/gerenciamento/GerenciamentoApplicationTests.java
+++ gerenciamento/src/test/java/ap1/bigdata/gerenciamento/GerenciamentoApplicationTests.java
@@ -1,13 +1,9 @@
 package ap1.bigdata.gerenciamento;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GerenciamentoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
